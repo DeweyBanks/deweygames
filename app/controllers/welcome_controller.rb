33a56474f91a21116d27f8467b9aacdf975a0b98
Dewey Banks <DeweyBanks@gmail.com>
2015-07-06 09:30:01 -0400

    fixed bug with user auth in welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,13 +9,9 @@ class WelcomeController < ActionController::Base
 
 protected
   def authenticate_user!
-    if logged_in?
-      super
-    else
+    unless logged_in?
       redirect_to login_path, :notice => 'You must be signed in to take the quiz'
-      ## if you want render 404 page
-      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
+    end
   end
-end
 
 end
