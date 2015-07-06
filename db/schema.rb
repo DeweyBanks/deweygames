cdc3967b5a8a6ff55dbafa221edfe9ab93611a83
Dewey Banks <DeweyBanks@gmail.com>
2015-07-06 09:51:22 -0400

    adds default to user admin, hides login if user logged in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150705161554) do
+ActiveRecord::Schema.define(version: 20150706134257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,10 +27,8 @@ ActiveRecord::Schema.define(version: 20150705161554) do
   create_table "users", force: :cascade do |t|
     t.string  "email"
     t.string  "name"
-    t.boolean "admin"
+    t.boolean "admin",           default: false
     t.string  "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
